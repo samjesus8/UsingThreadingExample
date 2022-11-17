b3e5c199c470f4f9b48d2e7fe20b8ee6e0cc5f28
sj481 <sj481@C60350098>
2022-11-17 11:06:39 +0000

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ThreadingExample
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.numberBox = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numberBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(79, 120);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(107, 48);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(217, 120);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(107, 48);
+            this.stopButton.TabIndex = 1;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number Input";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 78);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(373, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // numberBox
+            // 
+            this.numberBox.Location = new System.Drawing.Point(12, 42);
+            this.numberBox.Maximum = new decimal(new int[] {
+            91,
+            0,
+            0,
+            0});
+            this.numberBox.Name = "numberBox";
+            this.numberBox.Size = new System.Drawing.Size(120, 20);
+            this.numberBox.TabIndex = 5;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(397, 237);
+            this.Controls.Add(this.numberBox);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Name = "Main";
+            this.Text = "Fibonacci Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.numberBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.NumericUpDown numberBox;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace ThreadingExample
+{
+    public partial class Main : Form
+    {
+        private int numberToCompute = 0;
+        private int highestPercentageReached = 0;
+        public Main()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if(backgroundWorker1.IsBusy != true) 
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            if(backgroundWorker1.WorkerSupportsCancellation == true) 
+            {
+                backgroundWorker1.CancelAsync();
+            }
+        }
+
+        public long ComputeFibonacci(int n, BackgroundWorker worker, DoWorkEventArgs e)
+        {
+            // The parameter n must be >= 0 and <= 91.
+            // Fib(n), with n > 91, overflows a long.
+            if ((n < 0) || (n > 91))
+            {
+                throw new ArgumentException(
+                    "value must be >= 0 and <= 91", "n");
+            }
+
+            long result = 0;
+
+            // Abort the operation if the user has canceled.
+            // Note that a call to CancelAsync may have set 
+            // CancellationPending to true just after the
+            // last invocation of this method exits, so this 
+            // code will not have the opportunity to set the 
+            // DoWorkEventArgs.Cancel flag to true. This means
+            // that RunWorkerCompletedEventArgs.Cancelled will
+            // not be set to true in your RunWorkerCompleted
+            // event handler. This is a race condition.
+
+            if (worker.CancellationPending)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                if (n < 2)
+                {
+                    result = 1;
+                }
+                else
+                {
+                    result = ComputeFibonacci(n - 1, worker, e) +
+                             ComputeFibonacci(n - 2, worker, e);
+                }
+
+                // Report progress as a percentage of the total task.
+                int percentComplete =
+                    (int)((float)n / (float)numberToCompute * 100);
+                if (percentComplete > highestPercentageReached)
+                {
+                    highestPercentageReached = percentComplete;
+                    worker.ReportProgress(percentComplete);
+                }
+            }
+
+            return result;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            var result = ComputeFibonacci((int)numberBox.Value, worker, e);
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+    }
+}

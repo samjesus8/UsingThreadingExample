f8811ac291a5aa5de28c6d800bd0191e47c9db86
Samuel Jesuthas <SJ481@canterbury.ac.uk>
2022-11-17 12:01:26 +0000

    BackgroundWorker Thread

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -28,105 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.startButton = new System.Windows.Forms.Button();
-            this.stopButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.numberBox = new System.Windows.Forms.NumericUpDown();
-            this.outputBox = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.numberBox)).BeginInit();
+            this.backgroundThread = new System.Windows.Forms.Button();
+            this.threadPool = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // startButton
-            // 
-            this.startButton.Location = new System.Drawing.Point(79, 120);
-            this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(107, 48);
-            this.startButton.TabIndex = 0;
-            this.startButton.Text = "Start";
-            this.startButton.UseVisualStyleBackColor = true;
-            this.startButton.Click += new System.EventHandler(this.startButton_Click);
-            // 
-            // stopButton
-            // 
-            this.stopButton.Location = new System.Drawing.Point(217, 120);
-            this.stopButton.Name = "stopButton";
-            this.stopButton.Size = new System.Drawing.Size(107, 48);
-            this.stopButton.TabIndex = 1;
-            this.stopButton.Text = "Stop";
-            this.stopButton.UseVisualStyleBackColor = true;
-            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Font = new System.Drawing.Font("MS PGothic", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(92, 39);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Number Input";
+            this.label1.Size = new System.Drawing.Size(443, 97);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Threading";
             // 
-            // progressBar1
+            // backgroundThread
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 78);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(373, 23);
-            this.progressBar1.TabIndex = 4;
+            this.backgroundThread.Location = new System.Drawing.Point(122, 172);
+            this.backgroundThread.Name = "backgroundThread";
+            this.backgroundThread.Size = new System.Drawing.Size(119, 52);
+            this.backgroundThread.TabIndex = 1;
+            this.backgroundThread.Text = "Background Threading";
+            this.backgroundThread.UseVisualStyleBackColor = true;
+            this.backgroundThread.Click += new System.EventHandler(this.backgroundThread_Click);
             // 
-            // backgroundWorker1
+            // threadPool
             // 
-            this.backgroundWorker1.WorkerReportsProgress = true;
-            this.backgroundWorker1.WorkerSupportsCancellation = true;
-            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
-            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
-            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            this.threadPool.Location = new System.Drawing.Point(247, 172);
+            this.threadPool.Name = "threadPool";
+            this.threadPool.Size = new System.Drawing.Size(119, 52);
+            this.threadPool.TabIndex = 2;
+            this.threadPool.Text = "Thread Pool";
+            this.threadPool.UseVisualStyleBackColor = true;
+            this.threadPool.Click += new System.EventHandler(this.threadPool_Click);
             // 
-            // numberBox
+            // button3
             // 
-            this.numberBox.Location = new System.Drawing.Point(12, 42);
-            this.numberBox.Maximum = new decimal(new int[] {
-            91,
-            0,
-            0,
-            0});
-            this.numberBox.Name = "numberBox";
-            this.numberBox.Size = new System.Drawing.Size(120, 20);
-            this.numberBox.TabIndex = 5;
-            // 
-            // outputBox
-            // 
-            this.outputBox.Location = new System.Drawing.Point(202, 42);
-            this.outputBox.Name = "outputBox";
-            this.outputBox.Size = new System.Drawing.Size(100, 20);
-            this.outputBox.TabIndex = 6;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(202, 23);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(39, 13);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Output";
+            this.button3.Location = new System.Drawing.Point(372, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 52);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(397, 237);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.outputBox);
-            this.Controls.Add(this.numberBox);
-            this.Controls.Add(this.progressBar1);
+            this.ClientSize = new System.Drawing.Size(623, 277);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.threadPool);
+            this.Controls.Add(this.backgroundThread);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.stopButton);
-            this.Controls.Add(this.startButton);
             this.Name = "Main";
-            this.Text = "Fibonacci Calculator";
-            ((System.ComponentModel.ISupportInitialize)(this.numberBox)).EndInit();
+            this.Text = "Main";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,14 +92,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Button startButton;
-        private System.Windows.Forms.Button stopButton;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.NumericUpDown numberBox;
-        private System.Windows.Forms.TextBox outputBox;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button backgroundThread;
+        private System.Windows.Forms.Button threadPool;
+        private System.Windows.Forms.Button button3;
     }
 }
-
